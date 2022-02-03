b7c3f6e1aac044225de6bda0d84d008d8d2ff965
watcharin <watcharin.r@apollo21.asia>
2022-02-03 09:45:46 +0700

    pokemon

--- interface/selected.ts
+++
@@ -1,7 +0,0 @@
-export enum IOnSelected {
-    FORM_TYPE = 1,
-    POKEMON_TYPE = 2,
-    FUNCTION_ONE_TYPE = 3,
-    FUNCTION_TWO_TYPE = 4,
-    FUNCTION_THREE_TYPE = 5,
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
-import { useCallback, useState } from 'react'
-import { Container } from 'react-bootstrap'
+import { useCallback, useEffect, useState } from 'react'
+import { Col, Container, Navbar, Row } from 'react-bootstrap'
 import { IPokemonALLData } from '../interface/pokemon'
 
 import FormValidation from '../components/molecule/exam-validation'
@@ -8,12 +8,19 @@ import CategoryType from '../components/molecule/category/category'
 import Function3 from '../components/molecule/exam-function-3'
 import Function2 from '../components/molecule/exam-function-2'
 import Pokemon from '../components/oranism/pokemon'
-import { IOnSelected } from '../interface/selected'
 
-
+export enum IOnSelected {
+  FORM_TYPE = 1,
+  POKEMON_TYPE = 2,
+  FUNCTION_ONE_TYPE = 3,
+  FUNCTION_TWO_TYPE = 4,
+  FUNCTION_THREE_TYPE = 5,
+}
 
 const Home: NextPage = () => {
+  const [PokemonData, setPokemon] = useState<IPokemonALLData[]>([]);
   const [onSelected, setOnSelected] = useState<IOnSelected>(0)
+
 
   const callbackState = useCallback((stateData: IOnSelected) => {
     setOnSelected(stateData)
